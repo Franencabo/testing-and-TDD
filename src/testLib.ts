37ad239ec9cfa27ec66b56461c2672794975c374
Fran <franencabo@gmail.com>
2025-01-30 21:18:41 +0100

    My own test library

---
+++ src/testLib.ts
@@ -0,0 +1,30 @@
+export function expect<T>(expected: T) {
+    return {
+        toBe(result: T) {
+            if (result !== expected) {
+                throw new Error(`🚫 ${result} is not equal to ${expected}`)
+            }
+
+        }
+    }
+}
+
+
+export async function test(description:string, callback:()=>void){
+    try{
+        await callback();
+        console.log(`✅ ${description}`);
+    }
+    catch (error){
+        console.log(`🚫 ${description}`);
+        console.log(error);
+    }
+}
+
+
+export const it = test;
+
+export function describe(description: string, callback: ()=> void){
+    console.log(description);
+    callback();
+}
