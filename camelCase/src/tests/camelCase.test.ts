0111af3b6f28249ae8f4bd47d2177ba24e30b187
Fran <franencabo@gmail.com>
2025-02-04 21:36:43 +0100

    Kata Camelcase

---
+++ camelCase/src/tests/camelCase.test.ts
@@ -0,0 +1,23 @@
+import {toCamelCase} from "../core/camelCase";
+
+describe('Camel case converter', ()=>{
+    it('Allows empty text', ()=> {
+        expect(toCamelCase('')).toBe('')
+    })
+    it('Allows capitalized word', ()=>{
+    expect(toCamelCase('Foo')).toBe('Foo')
+    })
+    it('Joins the capitalized words that are separated by spaces', ()=>{
+        expect(toCamelCase('Foo Bar')).toBe('FooBar')
+    })
+    it('Joins the capitalized words that are separated by hyphens', ()=>{
+        expect(toCamelCase('Foo_Bar-Foo')).toBe('FooBarFoo')
+    })
+    it('Converts the first character of one word to uppercase', ()=>{
+        expect(toCamelCase('foo')).toBe('Foo')
+    })
+    it('Converts the first character of one word to uppercase', ()=>{
+        expect(toCamelCase('foo_bar_foo')).toBe('FooBarFoo')
+    })
+
+})
